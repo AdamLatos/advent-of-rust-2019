ddca03f870d24961d3b72ddc6b648f94ea3aeebe
AdamLatos <latosadam1@gmail.com>
2019-12-03 13:46:40 +0000

    correct program for part 1

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -8,10 +8,8 @@ fn main() {
     let path_1 = path_to_intermediate_points(paths.next().unwrap());
     let path_2 = path_to_intermediate_points(paths.next().unwrap());
 
-
-
-     let crossing = closest_crossing(path_1, path_2);
-     println!("The closest crossing distance: {}", crossing);
+    let crossing = closest_crossing(path_1, path_2);
+    println!("The closest crossing distance: {}", crossing);
 }
 
 #[derive(Debug, Copy, Clone, PartialEq)]
@@ -21,14 +19,13 @@ struct Point {
 }
 
 fn closest_crossing(path_1: Vec<Point>, path_2: Vec<Point>) -> i64 {
-    let origin = Point{x: 0, y: 0};
     let mut min_dist: i64 = std::i64::MAX;
 
-    println!("{}, {}", path_1.len(), path_2.len());
     for line1 in path_1.windows(2) {
         for line2 in path_2.windows(2) {
             match check_intersect(&line1[0], &line1[1], &line2[0], &line2[1]) {
                 Some(dist) => {
+                    // println!("Found crossing: {:?}", dist);
                     if dist < min_dist {
                         min_dist = dist
                     }
@@ -40,7 +37,7 @@ fn closest_crossing(path_1: Vec<Point>, path_2: Vec<Point>) -> i64 {
     min_dist
 }
 
-fn closest_crossing_naive(path_1: Vec<Point>, path_2: Vec<Point>) -> i64 {
+fn _closest_crossing_naive(path_1: Vec<Point>, path_2: Vec<Point>) -> i64 {
     let origin = Point{x: 0, y: 0};
     let mut min_dist: i64 = std::i64::MAX;
 
@@ -48,10 +45,8 @@ fn closest_crossing_naive(path_1: Vec<Point>, path_2: Vec<Point>) -> i64 {
     for p1 in path_1.iter() {
         for p2 in path_2.iter() {
             if p1 == p2 {
-                println!("Equal");
                 let dist = distance(p1, &origin);
                 if dist < min_dist {
-                    println!("New min dist: {}", dist);
                     min_dist = dist;
                 }
             }
@@ -85,19 +80,19 @@ fn check_intersect(p1: &Point, p2: &Point, p3: &Point, p4: &Point) -> Option<i64
     if p1.x <= p3.x && p2.x >= p4.x || p1.x >= p3.x && p2.x <= p4.x {
         if p1.y <= p3.y && p2.y >= p4.y || p1.y >= p3.y && p2.y <= p4.y {
             if p1.y == p2.y && p3.x == p4.x {
-                return Some(distance(&origin, &Point{x: p3.x, y: p4.y}));
+                return Some(distance(&origin, &Point{x: p3.x, y: p1.y}));
             }
             else if p1.x == p2.x && p3.y == p4.y {
                 return Some(distance(&origin, &Point{x: p1.x, y: p3.y}));
             }
-            //println!("Found intersection: {:?}, {:?}, {:?}, {:?}", p1, p2, p3, p4);
+            
             return Some(0)
         }
     }
     None
 }
 
-fn path_to_points(path: &str) -> Vec<Point> {
+fn _path_to_points(path: &str) -> Vec<Point> {
     let mut points: Vec<Point> = Vec::new();
     let mut ptr = Point{x: 0, y: 0};
     for inst in path.split(",") {
@@ -116,7 +111,6 @@ fn path_to_points(path: &str) -> Vec<Point> {
             points.push(ptr);
         }
     }
-    println!("done");
     points
 }
 
@@ -138,18 +132,18 @@ mod tests {
 
     #[test]
     fn test_crossing() {
-        let path_1 = path_to_points("R8,U5,L5,D3");
-        let path_2 = path_to_points("U7,R6,D4,L4");
+        let path_1 = _path_to_points("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51");
+        let path_2 = _path_to_points("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7");
 
-        assert_eq!(closest_crossing_naive(path_1, path_2), 6); 
+        assert_eq!(_closest_crossing_naive(path_1, path_2), 135); 
     }
 
     #[test]
     fn test_crossing_equivalency() {
-        let path_1 = path_to_points("R8,U5,L5,D3");
-        let path_2 = path_to_points("U7,R6,D4,L4");
+        let path_1 = _path_to_points("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51");
+        let path_2 = _path_to_points("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7");
 
-        assert_eq!(closest_crossing_naive(path_1.clone(), path_2.clone()), closest_crossing(path_1, path_2)); 
+        assert_eq!(_closest_crossing_naive(path_1.clone(), path_2.clone()), closest_crossing(path_1, path_2)); 
     }
 
     #[test]
