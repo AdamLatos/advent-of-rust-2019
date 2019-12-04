bce31ec7dc68d40fbbc0f15da230a11825d25bcb
AdamLatos <latosadam1@gmail.com>
2019-12-04 16:55:06 +0000

    solved 2nd task

--- day-04/src/main.rs
+++ day-04/src/main.rs
@@ -3,6 +3,7 @@ fn main() {
     let hi = 785961;
     let mut cnt = 0;
 
+    // 1
     for i in low..hi {
         let digits = digits(i);
         let mut stop = 0;
@@ -30,6 +31,42 @@ fn main() {
         //break;
     }
     println!("There are {} possibilities", cnt);
+
+    // 2
+    cnt = 0;
+    for i in low..hi {
+        let digits = digits(i);
+        let mut stop = 0;
+        for pair in digits.windows(2) {
+            if pair[0] > pair[1] {
+                stop = 1;
+                break;
+            }
+        }
+        if stop == 1 {
+            continue;
+        }
+        stop = 1;
+        if digits[0] == digits[1] && digits[1] != digits[2] {
+            stop = 0;
+        }
+        if digits[5] == digits[4] && digits[4] != digits[3] {
+            stop = 0;
+        }
+        for quad in digits.windows(4) {
+            if quad[0] != quad[1] && quad[1] == quad[2] && quad[2] != quad[3] {
+                stop = 0;
+                break;
+            }
+        }
+        if stop == 1 {
+            continue;
+        }
+        cnt += 1;
+        //println!("{}", i);
+        //break;
+    }
+    println!("Actually, there are {} possibilities", cnt);
 }
 
 fn digits(num: i32) -> Vec<i32> {
